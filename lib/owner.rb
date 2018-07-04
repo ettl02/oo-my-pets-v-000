bf4d8a3c00b06feb4ed602b4568dfd569b0c041e
Emily Wessel <ettl02@gmail.com>
2018-07-04 22:06:53 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -70,7 +70,7 @@ def sell_pets
    end
 
   def list_pets
-    "I have #{pets [:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
   end
 end
 # end
