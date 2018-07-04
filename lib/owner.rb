564ab23e86289f238ca4e738b8a6d6ae080f3e57
Emily Wessel <ettl02@gmail.com>
2018-07-04 21:54:10 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+OWNERS = []
+attr_accessor :name, :pets
+attr_reader :species
+
+def self.all
+    OWNERS
+  end
+
+  def initialize(species)
+   @species = species
+   OWNERS << self
+   @pets = {:fishes => [], :dogs => [], :cats => []}
+ end
+
+def self.all?
+  OWNERS
 end
+
+def self.reset_all
+  OWNERS.clear
+end
+
+#
+ def self.count
+   OWNERS.size
+ end
+# end
