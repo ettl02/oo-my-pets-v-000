9699cd487a19141b82378c991109be592283a4f1
Emily Wessel <ettl02@gmail.com>
2018-07-04 21:56:16 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,5 +33,13 @@ end
   def buy_fish(name)
     pets[:fishes] << Fish.new(name)
   end
+
+  def buy_dog(name)
+  pets[:dogs] << Dog.new(name)
+end
+
+def buy_cat(name)
+  pets[:cats] << Cat.new(name)
+end
  end
 # end
